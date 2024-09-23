dee486928801af0896c573339a9d7b921516ed78
Krushed Interactive Entertainment <mightymelvin2007@gmail.com>
2024-09-23 15:45:58 -0500

    a

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,17 @@ THIS WILL NOT BE THE BEST AND I KNOW IT CAN BE BETTER ;-;
 Also I'm using ListItem because idk how to use paragraph in ratatui
 */
 
+mod profiles;
+mod ucode;
+mod drivers;
 
-//mod profiles;
-
-//use profiles::{InstallProfile, install_profile};
-use ratatui::{backend::CrosstermBackend, Terminal};
-use ratatui::widgets::{List, ListItem, Block, Borders};
-use dialoguer::{Password, Input, Confirm};
-use std::io::{self, stdout};
+use profiles::{ InstallProfile, install_profile };
+// use ucode::{InstallUcode, install_ucode};
+// use drivers::{InstallDriver, install_driver};
+use ratatui::{ backend::CrosstermBackend, Terminal };
+use ratatui::widgets::{ List, ListItem, Block, Borders };
+use dialoguer::{ Password, Input, Confirm };
+use std::io::{ self, stdout };
 //use std::thread;
 
 struct InstallerState {
@@ -26,7 +29,7 @@ struct InstallerState {
     user_pass: String,
 }
 
-/* fn run_command(command: &str) {
+fn run_command(command: &str) {
     use std::process::Command;
     let output = Command::new("sh")
         .arg("-c")
@@ -38,12 +41,31 @@ struct InstallerState {
         println!("Command failed: {}", String::from_utf8_lossy(&output.stderr));
     }
 }
-    */
+
+fn chroot_command(_command: &str) {
+    use std::process::Command;
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("arch-chroot /mnt {_command}")
+        .output()
+        .expect("Failed to execute command");
+
+    if !output.status.success() {
+        println!("Command failed: {}", String::from_utf8_lossy(&output.stderr));
+    }
+}
 
 fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout());
     let mut terminal = Terminal::new(backend)?;
-    let mut state = InstallerState { selected_profile: 0, selected_ucode: 0, selected_driver: 0, root_pass: "".to_string(), username: "".to_string(), user_pass: "".to_string(), };
+    let mut state = InstallerState {
+        selected_profile: 0,
+        selected_ucode: 0,
+        selected_driver: 0,
+        root_pass: "".to_string(),
+        username: "".to_string(),
+        user_pass: "".to_string(),
+    };
     terminal.clear()?;
 
     println!("Hello, Welcome To The Krushed Arch Linux Installer (Made In Rust)");
@@ -58,53 +80,59 @@ fn main() -> Result<(), io::Error> {
     if !driveconfirmation {
         terminal.clear()?;
         println!(" Install... \nPlease set up your drives...");
-        return Ok(())
+        return Ok(());
     } else {
         profile_selector(&mut state)?;
     }
     Ok(())
 }
 
-
 fn profile_selector(state: &mut InstallerState) -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout());
     let mut terminal = Terminal::new(backend)?;
 
     terminal.clear()?;
-        // PROFILE SELECT
+    // PROFILE SELECT
 
-        let profile_choices_msg = vec![
-            ListItem::new("Please Type A Number To Select A Profile\n\n "),
-            ListItem::new("1. Base          \n(Pretty Much Nothing Is Installed.)\n "),
-            ListItem::new("2. Minimal       \n(Best For Servers.)\n "),
-            ListItem::new("3. Desktop       \n(Minimal Desktop Env.)\n "),
-            ListItem::new("4. Full Desktop  \n(Has Apps Like VLC, GPARTED, LIBREOFFICE, and CODE.)\n "),
-            ListItem::new("5. Gaming        \n(Pre Installed Wine Staging , Steam, Lutris, And Other Gaming Packages.)\n "),
-        ];
+    let profile_choices_msg = vec![
+        ListItem::new("Please Type A Number To Select A Profile\n\n "),
+        ListItem::new("1. Base"),
+        ListItem::new("(Pretty Much Nothing Is Installed)"),
+        ListItem::new(""),
+        ListItem::new("2. Minimal"),
+        ListItem::new("(Best For Servers)"),
+        ListItem::new(""),
+        ListItem::new("3. Desktop"),
+        ListItem::new("(Minimal Desktop Env)"),
+        ListItem::new(""),
+        ListItem::new("4. Full Desktop"),
+        ListItem::new("(Has Apps Like VLC, GPARTED, LIBREOFFICE, and CODE.)"),
+        ListItem::new(""),
+        ListItem::new("5. Gaming"),
+        ListItem::new("(Pre Installed Wine Staging , Steam, Lutris, And Other Gaming Packages.)"),
+        ListItem::new("")
+    ];
 
-        let profile_list = List::new(profile_choices_msg)
-            .block(Block::default().borders(Borders::ALL));
+    let profile_list = List::new(profile_choices_msg).block(Block::default().borders(Borders::ALL));
 
-        terminal.draw(|frame| {
-            let size = frame.area();
-            frame.render_widget(profile_list, size);
-        })?;
+    terminal.draw(|frame| {
+        let size = frame.area();
+        frame.render_widget(profile_list, size);
+    })?;
 
-        let selected_profile = Input::<i32>::new()
-            .interact_text()
-            .unwrap();
-        terminal.clear()?;
+    let selected_profile = Input::<i32>::new().interact_text().unwrap();
+    terminal.clear()?;
 
-        if selected_profile >= 1 && selected_profile <= 5 {
-            println!("{selected_profile}");
-            state.selected_profile = selected_profile;
-            ucode_selector(state)?;
+    if selected_profile >= 1 && selected_profile <= 5 {
+        println!("{selected_profile}");
+        state.selected_profile = selected_profile;
+        ucode_selector(state)?;
 
-            Ok(())
-        } else {
-            println!("Invalid Profile Choice, Please Enter A Number Between 1 - 5");
-            return Ok(())
-        }
+        Ok(())
+    } else {
+        println!("Invalid Profile Choice, Please Enter A Number Between 1 - 5");
+        return Ok(());
+    }
 }
 
 fn ucode_selector(state: &mut InstallerState) -> Result<(), io::Error> {
@@ -116,20 +144,17 @@ fn ucode_selector(state: &mut InstallerState) -> Result<(), io::Error> {
     let ucode_choices_msg = vec![
         ListItem::new("Please Type A Number To Select A CPU Platform\n\n "),
         ListItem::new("1. INTEL"),
-        ListItem::new("2. AMD"),
+        ListItem::new("2. AMD")
     ];
 
-    let ucode_list = List::new(ucode_choices_msg)
-        .block(Block::default().borders(Borders::ALL));
+    let ucode_list = List::new(ucode_choices_msg).block(Block::default().borders(Borders::ALL));
 
     terminal.draw(|frame| {
         let size = frame.area();
         frame.render_widget(ucode_list, size);
     })?;
 
-    let selected_ucode = Input::<i32>::new()
-        .interact_text()
-        .unwrap();
+    let selected_ucode = Input::<i32>::new().interact_text().unwrap();
     terminal.clear()?;
 
     if selected_ucode >= 1 && selected_ucode <= 2 {
@@ -139,7 +164,7 @@ fn ucode_selector(state: &mut InstallerState) -> Result<(), io::Error> {
         Ok(())
     } else {
         println!("Invalid CPU Choice.");
-        return Ok(())
+        return Ok(());
     }
 }
 
@@ -154,20 +179,17 @@ fn driver_selector(state: &mut InstallerState) -> Result<(), io::Error> {
         ListItem::new("1. AMD"),
         ListItem::new("2. NVIDIA"),
         ListItem::new("3. INTEL"),
-        ListItem::new("4. VMWARE"),
+        ListItem::new("4. VMWARE")
     ];
 
-    let driver_list = List::new(driver_choices_msg)
-        .block(Block::default().borders(Borders::ALL));
+    let driver_list = List::new(driver_choices_msg).block(Block::default().borders(Borders::ALL));
 
     terminal.draw(|frame| {
         let size = frame.area();
         frame.render_widget(driver_list, size);
     })?;
 
-    let selected_driver = Input::<i32>::new()
-        .interact_text()
-        .unwrap();
+    let selected_driver = Input::<i32>::new().interact_text().unwrap();
     terminal.clear()?;
 
     if selected_driver >= 1 && selected_driver <= 4 {
@@ -175,7 +197,7 @@ fn driver_selector(state: &mut InstallerState) -> Result<(), io::Error> {
         root_password(state)
     } else {
         println!("Invalid CPU Choice.");
-        return Ok(())
+        return Ok(());
     }
 }
 
@@ -185,12 +207,9 @@ fn root_password(state: &mut InstallerState) -> Result<(), io::Error> {
     terminal.clear()?;
 
     // UCODE SELECT
-    let root_choices_msg = vec![
-        ListItem::new("Please Set A Root Password\n\n "),
-    ];
+    let root_choices_msg = vec![ListItem::new("Please Set A Root Password\n\n ")];
 
-    let root_list = List::new(root_choices_msg)
-        .block(Block::default().borders(Borders::ALL));
+    let root_list = List::new(root_choices_msg).block(Block::default().borders(Borders::ALL));
 
     terminal.draw(|frame| {
         let size = frame.area();
@@ -217,20 +236,17 @@ fn user_name(state: &mut InstallerState) -> Result<(), io::Error> {
     // UCODE SELECT
     let user_choices_msg = vec![
         ListItem::new("Please Type A Username\n\n "),
-        ListItem::new("Note This User Will Be Added To The \"Wheel\" Group"),
+        ListItem::new("Note This User Will Be Added To The \"Wheel\" Group")
     ];
 
-    let user_list = List::new(user_choices_msg)
-        .block(Block::default().borders(Borders::ALL));
+    let user_list = List::new(user_choices_msg).block(Block::default().borders(Borders::ALL));
 
     terminal.draw(|frame| {
         let size = frame.area();
         frame.render_widget(user_list, size);
     })?;
 
-    let user_na = Input::new()
-        .interact()
-        .unwrap();
+    let user_na = Input::new().interact().unwrap();
     terminal.clear()?;
 
     state.username = user_na;
@@ -245,12 +261,9 @@ fn user_password(state: &mut InstallerState) -> Result<(), io::Error> {
     terminal.clear()?;
 
     // UCODE SELECT
-    let user_choices_msg = vec![
-        ListItem::new("Please Set A User Password"),
-    ];
+    let user_choices_msg = vec![ListItem::new("Please Set A User Password")];
 
-    let user_list = List::new(user_choices_msg)
-        .block(Block::default().borders(Borders::ALL));
+    let user_list = List::new(user_choices_msg).block(Block::default().borders(Borders::ALL));
 
     terminal.draw(|frame| {
         let size = frame.area();
@@ -270,6 +283,73 @@ fn user_password(state: &mut InstallerState) -> Result<(), io::Error> {
     println!("ROOT PASSWORD: {}", state.root_pass);
     println!("USERNAME: {}", state.username);
     println!("USER PASS: {}", state.user_pass);
+    install_confirm(state)?;
+    Ok(())
+}
 
+fn install_confirm(state: &mut InstallerState) -> Result<(), io::Error> {
+    let backend = CrosstermBackend::new(stdout());
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+
+    let install_confirmation = Confirm::new()
+        .with_prompt("Are you sure you want to continue with this install?")
+        .default(true)
+        .interact()
+        .unwrap();
+
+    if !install_confirmation {
+        terminal.clear()?;
+        println!(" Install Cancelled...");
+        return Ok(());
+    } else {
+        start_install(state)?;
+    }
+
+    Ok(())
+}
+
+fn start_install(state: &mut InstallerState) -> Result<(), io::Error> {
+    let backend = CrosstermBackend::new(stdout());
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+    let mut chosen_profile;
+    // let mut chosen_ucode = InstallProfile::Base;
+    // let mut chosen_driver = InstallProfile::Base;
+    // let mut chosen_root_password = state.root_pass;
+    // let mut chosen_username = state.username;
+    // let mut chosen_user_password = state.user_pass;
+
+    match state.selected_profile {
+        1 => {
+            chosen_profile = InstallProfile::Base;
+            install_profile(chosen_profile);
+        }
+        2 => {
+            chosen_profile = InstallProfile::Minimal;
+            install_profile(chosen_profile);
+        }
+        3 => {
+            chosen_profile = InstallProfile::Desktop;
+            install_profile(chosen_profile);
+        }
+        4 => {
+            chosen_profile = InstallProfile::FullDesktop;
+            install_profile(chosen_profile);
+        }
+        5 => {
+            chosen_profile = InstallProfile::Gaming;
+            install_profile(chosen_profile);
+        }
+        _ => {
+            println!("A Weird Error Happened And I Didn't Remeber What Profile You Selected...");
+        }
+    }
+
+    Ok(())
+}
+
+fn chroot_actions(state: &mut InstallerState) -> Result<(), io::Error> {
+    println!("Chroot Actions...");
     Ok(())
 }
--- src/profiles.rs
+++ src/profiles.rs
@@ -10,14 +10,14 @@ Base (Base Arch Linux. Almost Nothing Installed):
     grub 
     efibootmgr 
     openssh 
-    networkmanager 
+    networkmanager
+    vim
+    git 
     UCODE
 
 
 Minimal (Minimal Stuff Installed):
     os-prober
-    vim
-    git
     fastfetch
     btop
     ly
@@ -119,8 +119,6 @@ Gaming (Full KDE Desktop Gaming Environment With Preinstalled Wine-Staging And O
 
 use indicatif::{ProgressBar, ProgressStyle};
 use std::process::{Command, Stdio};
-use std::thread;
-use std::sync::mpsc;
 
 pub enum InstallProfile {
     Base,
@@ -146,8 +144,70 @@ fn run_command(command: &str) {
 pub fn install_profile(profile: InstallProfile) {
     match profile {
         InstallProfile::Base => {
-            println!("Installing base profile...");
-            run_command("pacstrap -K -P /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr openssh networkmanager reflector")
+            // Base Install
+            println!("Installing Base Profile...");
+            //run_command("pacstrap -K -P /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr openssh networkmanager vim git");
         }
+        InstallProfile::Minimal => {
+            // Base Install
+            println!("Installing Base Profile...");
+            //run_command("pacstrap -K -P /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr openssh networkmanager vim git");
+
+            // Minimal Install
+            println!("Installing Minimal Profile...");
+            //run_command("pacstrap -K -P /mnt os-prober fastfetch btop ly reflector ldns wget curl xclip unzip unrar btrfs-progs exfat-utils ntfs-3g");
+        }
+        InstallProfile::Desktop => {
+            // Base Install
+            println!("Installing Base Profile...");
+            //run_command("pacstrap -K -P /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr openssh networkmanager vim git");
+
+            // Minimal Install
+            println!("Installing Minimal Profile...");
+            //run_command("pacstrap -K -P /mnt os-prober fastfetch btop ly reflector ldns wget curl xclip unzip unrar btrfs-progs exfat-utils ntfs-3g");
+
+            //Desktop Install
+            println!("Installing Desktop Profile...");
+            //run_command("pacstrap -K -P /mnt xorg wayland plasma firefox pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse noto-fonts konsole dolphin");
+        }
+        InstallProfile::FullDesktop  => {
+            // Base Install
+            println!("Installing Base Profile...");
+            //run_command("pacstrap -K -P /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr openssh networkmanager vim git");
+
+            // Minimal Install
+            println!("Installing Minimal Profile...");
+            //run_command("pacstrap -K -P /mnt os-prober fastfetch btop ly reflector ldns wget curl xclip unzip unrar btrfs-progs exfat-utils ntfs-3g");
+
+            //Desktop Install
+            println!("Installing Desktop Profile...");
+            //run_command("pacstrap -K -P /mnt xorg wayland plasma firefox pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse noto-fonts konsole dolphin");
+
+            //Full Desktop Install
+            println!("Installing Full Desktop Profile...");
+            //run_command("pacstrap -K -P /mnt zsh noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-hack-nerd gparted gvfs gvfs-afc grub-customizer flatpak dpkg less qpwgraph gnome-calculator fzf fuse2 fuse3 alsa-utils ufw vlc libreoffice-fresh code kvantum ocs-url");
+        }
+        InstallProfile::Gaming => {
+            // Base Install
+            println!("Installing Base Profile...");
+            //run_command("pacstrap -K -P /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr openssh networkmanager vim git");
+
+            // Minimal Install
+            println!("Installing Minimal Profile...");
+            //run_command("pacstrap -K -P /mnt os-prober fastfetch btop ly reflector ldns wget curl xclip unzip unrar btrfs-progs exfat-utils ntfs-3g");
+
+            //Desktop Install
+            println!("Installing Desktop Profile...");
+            //run_command("pacstrap -K -P /mnt xorg wayland plasma firefox pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse noto-fonts konsole dolphin");
+
+            //Full Desktop Install
+            println!("Installing Full Desktop Profile...");
+            //run_command("pacstrap -K -P /mnt zsh noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-hack-nerd gparted gvfs gvfs-afc grub-customizer flatpak dpkg less qpwgraph gnome-calculator fzf fuse2 fuse3 alsa-utils ufw vlc libreoffice-fresh code kvantum ocs-url");
+
+            //Gaming Install
+            println!("Installing Gaming Profile...");
+            //run_command("pacstrap -K -P /mnt steam discord lutris wine-staging giflib lib32-giflib gnutls lib32-gnutls v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib sqlite lib32-sqlite libxcomposite lib32-libxcomposite ocl-icd lib32-ocl-icd libva lib32-lbva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader sdl2 lib32-sdl2 jre-openjdk jre8-openjdk jre11-openjdk jre17-openjdk jre21-openjdk");
+        }
+
     }
 }
