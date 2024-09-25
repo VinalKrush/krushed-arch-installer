6b170bbb8cfd07f11f4e1647f5ca7a129154d20a
Krushed Interactive Entertainment <mightymelvin2007@gmail.com>
2024-09-25 18:49:47 -0500

    Optimizing profiles.rs

--- src/profiles.rs
+++ src/profiles.rs
@@ -141,99 +141,69 @@ fn run_command(command: &str) {
 pub fn install_profile(profile: InstallProfile) {
     match profile {
         InstallProfile::Base => {
-            // Base Install
-            println!("Installing Base Profile...");
-            run_command(
-                "pacstrap -K -P /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr openssh networkmanager vim git"
-            );
+            base_profile();
         }
         InstallProfile::Minimal => {
-            // Base Install
-            println!("Installing Base Profile...");
-            run_command(
-                "pacstrap -K -P /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr openssh networkmanager vim git"
-            );
-
-            // Minimal Install
-            println!("Installing Minimal Profile...");
-            run_command(
-                "pacstrap -K -P /mnt os-prober fastfetch btop ly reflector ldns wget curl xclip unzip unrar btrfs-progs exfat-utils ntfs-3g"
-            );
+            base_profile();
+            minimal_profile();
         }
         InstallProfile::Desktop => {
-            // Base Install
-            println!("Installing Base Profile...");
-            run_command(
-                "pacstrap -K -P /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr openssh networkmanager vim git"
-            );
-
-            // Minimal Install
-            println!("Installing Minimal Profile...");
-            run_command(
-                "pacstrap -K -P /mnt os-prober fastfetch btop ly reflector ldns wget curl xclip unzip unrar btrfs-progs exfat-utils ntfs-3g"
-            );
-
-            //Desktop Install
-            println!("Installing Desktop Profile...");
-            run_command(
-                "pacstrap -K -P /mnt xorg wayland plasma firefox pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse noto-fonts konsole dolphin"
-            );
+            base_profile();
+            minimal_profile();
+            desktop_profile();
         }
         InstallProfile::FullDesktop => {
-            // Base Install
-            println!("Installing Base Profile...");
-            run_command(
-                "pacstrap -K -P /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr openssh networkmanager vim git"
-            );
-
-            // Minimal Install
-            println!("Installing Minimal Profile...");
-            run_command(
-                "pacstrap -K -P /mnt os-prober fastfetch btop ly reflector ldns wget curl xclip unzip unrar btrfs-progs exfat-utils ntfs-3g"
-            );
-
-            //Desktop Install
-            println!("Installing Desktop Profile...");
-            run_command(
-                "pacstrap -K -P /mnt xorg wayland plasma firefox pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse noto-fonts konsole dolphin"
-            );
-
-            //Full Desktop Install
-            println!("Installing Full Desktop Profile...");
-            run_command(
-                "pacstrap -K -P /mnt zsh noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-hack-nerd gparted gvfs gvfs-afc grub-customizer flatpak dpkg less qpwgraph gnome-calculator fzf fuse2 fuse3 alsa-utils ufw vlc libreoffice-fresh code kvantum ocs-url"
-            );
+            base_profile();
+            minimal_profile();
+            desktop_profile();
+            full_desktop_profile();
         }
         InstallProfile::Gaming => {
-            // Base Install
-            println!("Installing Base Profile...");
-            run_command(
-                "pacstrap -K -P /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr openssh networkmanager vim git"
-            );
-
-            // Minimal Install
-            println!("Installing Minimal Profile...");
-            run_command(
-                "pacstrap -K -P /mnt os-prober fastfetch btop ly reflector ldns wget curl xclip unzip unrar btrfs-progs exfat-utils ntfs-3g"
-            );
-
-            //Desktop Install
-            println!("Installing Desktop Profile...");
-            run_command(
-                "pacstrap -K -P /mnt xorg wayland plasma firefox pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse noto-fonts konsole dolphin"
-            );
-
-            //Full Desktop Install
-            println!("Installing Full Desktop Profile...");
-            run_command(
-                "pacstrap -K -P /mnt zsh noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-hack-nerd gparted gvfs gvfs-afc grub-customizer flatpak dpkg less qpwgraph gnome-calculator fzf fuse2 fuse3 alsa-utils ufw vlc libreoffice-fresh code kvantum ocs-url"
-            );
-
-            //Gaming Install
-            println!("Installing Gaming Profile...");
-            run_command(
-                "pacstrap -K -P /mnt steam discord lutris wine-staging giflib lib32-giflib gnutls lib32-gnutls v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib sqlite lib32-sqlite libxcomposite lib32-libxcomposite ocl-icd lib32-ocl-icd libva lib32-lbva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader sdl2 lib32-sdl2 jre-openjdk jre8-openjdk jre11-openjdk jre17-openjdk jre21-openjdk"
-            );
+            base_profile();
+            minimal_profile();
+            desktop_profile();
+            full_desktop_profile();
+            gaming_profile();
         }
     }
+}
+
+fn base_profile() {
+    //Base Install
+    println!("Installing Base Packages...");
+    run_command(
+        "pacstrap -K -P /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr openssh networkmanager vim git"
+    );
+}
+
+fn minimal_profile() {
+    //Minimal Install
+    println!("Installing Minimal Packages...");
+    run_command(
+        "pacstrap -K -P /mnt os-prober fastfetch btop ly reflector ldns wget curl xclip unzip unrar btrfs-progs exfat-utils ntfs-3g"
+    );
+}
+
+fn desktop_profile() {
+    //Desktop Install
+    println!("Installing Desktop Packages...");
+    run_command(
+        "pacstrap -K -P /mnt xorg wayland plasma firefox pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse noto-fonts konsole dolphin"
+    );
+}
+
+fn full_desktop_profile() {
+    //Full Desktop Install
+    println!("Installing What Some People May Consider Bloat Packages...");
+    run_command(
+        "pacstrap -K -P /mnt zsh noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-hack-nerd gparted gvfs gvfs-afc grub-customizer flatpak dpkg less qpwgraph gnome-calculator fzf fuse2 fuse3 alsa-utils ufw vlc libreoffice-fresh code kvantum"
+    );
+}
+
+fn gaming_profile() {
+    //Gaming Install
+    println!("Installing Gaming Dependencies...");
+    run_command(
+        "pacstrap -K -P /mnt steam discord lutris wine-staging giflib lib32-giflib gnutls lib32-gnutls v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib sqlite lib32-sqlite libxcomposite lib32-libxcomposite ocl-icd lib32-ocl-icd libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader sdl2 lib32-sdl2 jre-openjdk jre8-openjdk jre11-openjdk jre17-openjdk jre21-openjdk"
+    );
 }
