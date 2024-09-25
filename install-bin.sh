f34719659169d6d6726d47f5f914c1e9b8da50c5
Krushed Interactive Entertainment <mightymelvin2007@gmail.com>
2024-09-25 17:57:11 -0500

    Added bin

--- install-bin.sh
+++ install-bin.sh
@@ -14,13 +14,12 @@ echo "Installing dependencies..."
 # Install Rust if not already installed
 if ! command -v rustc &> /dev/null; then
   pacman -S rustup
-  rustup default stable
 fi
+rustup default stable
 
 # Compile your Rust program
-cargo build --release
 mkdir -p /etc/krushed/arch-installer
 cp -r src/etc/* /etc/krushed/arch-installer/
 
 # Install the compiled binary
-install -Dm 755 target/release/krushed-arch-installer /usr/bin/krushed-arch-installer
+install -Dm 755 bin/krushed-arch-installer /usr/bin/krushed-arch-installer
