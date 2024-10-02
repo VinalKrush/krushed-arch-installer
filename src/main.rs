51d61b3c6be2195b0f6de4342e13cddb4e9eadea
Krushed Interactive Entertainment <mightymelvin2007@gmail.com>
2024-10-02 14:51:23 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -479,8 +479,7 @@ fn start_install(state: &mut InstallerState) -> Result<(), io::Error> {
         chroot_command("mkswap /swapfile");
         chroot_command("swapon /swapfile");
         let mut fstab_file = OpenOptions::new().write(true).append(true).open("/mnt/etc/fstab");
-        fstab_file?.write("\n".as_bytes());
-        fstab_file.clone()?.write("/swapfile swap swap defaults 0 0".as_bytes());
+        fstab_file?.write("/swapfile swap swap defaults 0 0".as_bytes());
     }
 
     println!("Setting Hostname...");
